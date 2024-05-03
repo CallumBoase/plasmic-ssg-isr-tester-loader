9824a0fafe51ad0cbda31cfeaa12d24f8d8eb3a1
CallumBoase <callum.boase@gmail.com>
2024-05-03 15:30:05 +1000

    toc

--- components/RenderMarkdown/index.tsx
+++ components/RenderMarkdown/index.tsx
@@ -4,6 +4,7 @@ import remarkGfm from "remark-gfm";
 import remarkToc from "remark-toc";
 import rehypeHighlight from "rehype-highlight";
 import rehypeRaw from "rehype-raw";
+import rehypeSlug from "rehype-slug";
 
 export type RenderMarkdownProps = {
   markdownText: string;
@@ -11,31 +12,35 @@ export type RenderMarkdownProps = {
   className?: string;
 };
 
-export function RenderMarkdown({ markdownText, dangerouslyRenderHtmlTags, className }: RenderMarkdownProps) {
-
+export function RenderMarkdown({
+  markdownText,
+  dangerouslyRenderHtmlTags,
+  className,
+}: RenderMarkdownProps) {
   //The component can accept URI encoded content
   //We decode it here
   //It doesn't matter if the content is not encoded, the decoding won't affect it
-  const finalMarkdown = decodeURIComponent(markdownText)
+  const finalMarkdown = decodeURIComponent(markdownText);
 
-  if( dangerouslyRenderHtmlTags === true ) {
+  if (dangerouslyRenderHtmlTags === true) {
     return (
-      <Markdown className={`markdown-render ${className}`}
+      <Markdown
+        className={`markdown-render ${className}`}
         remarkPlugins={[remarkGfm, remarkToc]}
-        rehypePlugins={[rehypeHighlight, rehypeRaw]}
+        rehypePlugins={[rehypeHighlight, rehypeRaw, rehypeSlug]}
       >
         {finalMarkdown}
       </Markdown>
     );
   } else {
     return (
-      <Markdown className={`markdown-render ${className}`}
+      <Markdown
+        className={`markdown-render ${className}`}
         remarkPlugins={[remarkGfm, remarkToc]}
-        rehypePlugins={[rehypeHighlight]}
+        rehypePlugins={[rehypeHighlight, rehypeSlug]}
       >
         {finalMarkdown}
       </Markdown>
     );
   }
-
 }
